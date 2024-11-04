aa41bb409f3854d13d34db4f351ba8a35d879849
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2024-11-04 21:49:23 +0600

    Specialist section introduced

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -2,11 +2,13 @@
 So that it  takes a common layout, But when i create 
 another page.tsx, it will not take common layout */
 import HeroSection from "@/components/UI/HomePage/heroSection/heroSection";
+import Specialist from "@/components/UI/Specialist/Specialist";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
+      <Specialist />
     </div>
   );
 };
---
+++ src/components/UI/Specialist/Specialist.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Specialist = () => {
+    return (
+        <div>
+            Specialist
+        </div>
+    );
+};
+
+export default Specialist;
